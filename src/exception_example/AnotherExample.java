d3549730ec5b0a37b7920b8683d60f9fa8e45a48
MaximGerasimenko93 <gerasimx93@gmail.com>
2023-04-04 22:40:40 +0700

    Добавил метод closeResources в нем сделал блок try-with-resources. В нем создал экземпляр класса FileReader

--- src/exception_example/AnotherExample.java
+++ src/exception_example/AnotherExample.java
@@ -5,7 +5,6 @@ public class AnotherExample {
     public AnotherExample() {
     }
 
-    // создаем метод, который выбрасывает Runtime исключение
     public void makeUnsafeSituation() {
         throw new RuntimeException("This is unsafe situation. Please sit and cry :)");
     }
--- src/exception_example/Example.java
+++ src/exception_example/Example.java
@@ -1,15 +1,11 @@
 package exception_example;
 
-
-// Наследуем класс Exception
 public class Example extends Exception {
 
-    // Создаем конструктор для отправки сообщения
     public Example(String message) {
         super(message);
     }
 
-    // Создаем конструктор для проброса причины исключения
     public Example(Throwable cause) {
         super(cause);
     }
